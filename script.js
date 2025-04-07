55e1837ebebfa863668302df9bd3fd29f63d3cff
T6nu <74606960+T6nuk@users.noreply.github.com>
2025-04-07 13:29:55 +0000

    Add html buttons, operate and populateDisplay functions

--- script.js
+++ script.js
@@ -23,3 +23,70 @@ const multiply = function (a, b) {
 const divide = function (a, b) {
   return a / b;
 };
+
+const num1 = null;
+const num2 = null;
+const operator = null;
+
+const operate = function (operator, num1, num2) {
+  switch (operator) {
+    case "+":
+      return add(num1, num2);
+    case "-":
+      return subtract(num1, num2);
+    case "*":
+      return multiply(num1, num2);
+    case "/":
+      return divide(num1, num2);
+    default:
+      break;
+  }
+};
+
+function populateDisplay() {
+  let screen = document.querySelector("#screen");
+  let btnAll = document.querySelectorAll("#calc-buttons");
+  let btnNumbers = document.querySelectorAll(".button-numbers > button");
+  let btnOperators = document.querySelectorAll(".button-operators > button");
+  let btnResult = document.querySelector("#button-equals");
+  let firstNum = [];
+  let secondNum = [];
+  let operator = "";
+
+  let btnNumbersValue = btnNumbers.forEach((button) => {
+    button.addEventListener("click", () => {
+      let pressedBtn = Number(button.textContent);
+      screen.textContent = pressedBtn;
+
+      if (operator == "") firstNum.push(pressedBtn);
+      else secondNum.push(pressedBtn);
+      return pressedBtn;
+    });
+  });
+
+  let btnOperatorsValue = btnOperators.forEach((button) => {
+    button.addEventListener("click", () => {
+      let pressedBtn = button.textContent;
+      screen.textContent = pressedBtn;
+      operator = pressedBtn;
+    });
+  });
+
+  let getResult = btnResult.addEventListener("click", () => {
+    let num1 = firstNum[firstNum.length - 1];
+    let num2 = secondNum[secondNum.length - 1];
+    console.log(num1);
+    console.log(operator);
+    console.log(num2);
+
+    let result = operate(operator, num1, num2);
+    console.log(result);
+
+    screen.textContent = result;
+    firstNum = [];
+    secondNum = [];
+    operator = "";
+  });
+}
+
+populateDisplay();
