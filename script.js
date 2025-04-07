b7b31ce90134aa6483492aff4d7c9520431123d7
T6nu <74606960+T6nuk@users.noreply.github.com>
2025-04-07 11:51:41 +0000

    Add, subtract, multiply, divide functions

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
 console.log(`test`);
+
+// add
+
+const add = function (a, b) {
+  return a + b;
+};
+
+// subtract
+
+const subtract = function (a, b) {
+  return a - b;
+};
+
+// multiply
+
+const multiply = function (a, b) {
+  return a * b;
+};
+
+// divide
+
+const divide = function (a, b) {
+  return a / b;
+};
